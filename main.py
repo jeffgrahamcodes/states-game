77a24a9f50f97df6ea71c9befddecb25c3367355
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-21 11:37:01 -0400

    add Cursor class

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+
 import turtle
+import pandas as pd
+
+from cursor import Cursor
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,4 +10,20 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-enter_state = screen.textinput("Guess the State", "Name another state")
+cursor = Cursor()
+
+df= pd.read_csv("50_states.csv")
+states_list = df["state"].to_list()
+
+guess = screen.textinput("Guess the State", "Name another state")
+formatted_guess = guess.title()
+
+if formatted_guess in states_list:
+    coordinates = df[df["state"] == formatted_guess]
+    x = coordinates.x.to_list()[0]
+    y = coordinates.y.to_list()[0]
+
+    cursor.move_to(x,y)
+    cursor.write_state(formatted_guess)
+
+screen.exitonclick()
